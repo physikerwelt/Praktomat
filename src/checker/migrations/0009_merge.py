5b9940213046aae2ee40f93b788e27fd7f203804
Moritz Schubotz <github@ckurs.de>
2017-05-12 10:19:40 +0200

    Confirm migration merges

---
+++ src/checker/migrations/0009_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('checker', '0008_auto_20170512_0624'),
+        ('checker', '0008_javachecker'),
+    ]
+
+    operations = [
+    ]
